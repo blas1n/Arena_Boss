d82892a8d6cbb1903873d403a6dab37d7ac51396
blAs1N <qazasa123@gmail.com>
2020-01-19 23:05:21 +0900

    Build scalar class.

---
+++ Arena_Boss/Scalar.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "pch.h"
+
+namespace Math
+{
+    class Scalar
+    {
+    public:
+        Scalar(float f = 0.0f) { value = DirectX::XMVectorReplicate(f); }
+        explicit Scalar(DirectX::XMVECTOR vec) { value = vec; }
+
+        Scalar(const Scalar&) = default;
+        inline Scalar& operator=(const Scalar&) = default;
+        ~Scalar() = default;
+
+        inline operator DirectX::XMVECTOR() const noexcept { return value; }
+        inline operator float() const noexcept { return DirectX::XMVectorGetX(value); }
+
+        inline Scalar& operator+=(const Scalar& other)
+        {
+            value = DirectX::XMVectorAdd(value, other);
+            return *this;
+        }
+
+        inline Scalar& operator-=(const Scalar& other)
+        {
+            value = DirectX::XMVectorSubtract(value, other);
+            return *this;
+        }
+
+        inline Scalar& operator*=(const Scalar& other)
+        {
+            value = DirectX::XMVectorMultiply(value, other);
+            return *this;
+        }
+
+        inline Scalar& operator/=(const Scalar& other)
+        {
+            value = DirectX::XMVectorDivide(value, other);
+            return *this;
+        }
+
+    private:
+        DirectX::XMVECTOR value;
+    };
+
+    inline Scalar operator-(const Scalar& s) { return Scalar{ DirectX::XMVectorNegate(s) }; }
+
+    inline Scalar operator+(const Scalar& lhs, const Scalar& rhs) { return Scalar{ lhs } += rhs; }
+    inline Scalar operator-(const Scalar& lhs, const Scalar& rhs) { return Scalar{ lhs } -= rhs; }
+    inline Scalar operator*(const Scalar& lhs, const Scalar& rhs) { return Scalar{ lhs } *= rhs; }
+    inline Scalar operator/(const Scalar& lhs, const Scalar& rhs) { return Scalar{ lhs } /= rhs; }
+}
