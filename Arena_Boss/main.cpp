307c93f14ef62e47deabc0797903eba2600895a7
blAs1N <qazasa123@gmail.com>
2020-01-20 23:37:30 +0900

    Build main function.

---
+++ Arena_Boss/main.cpp
@@ -0,0 +1,7 @@
+#include "Common.h"
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	MessageBox(nullptr, TEXT("Welcome to Arena Boss!"), TEXT("Welcome"), MB_OK);
+	return 0;
+}
