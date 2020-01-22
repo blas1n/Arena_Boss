f9c8849a664c75857df337e1299e798e6524c728
blAs1N <qazasa123@gmail.com>
2020-01-22 23:38:00 +0900

    Add tstring

--- Arena_Boss/Common.h
+++ Arena_Boss/Common.h
@@ -3,3 +3,10 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #include <tchar.h>
+#include <string>
+
+#ifdef _UNICODE
+using tstring = std::wstring;
+#else
+using tstring = std::string;
+#endif
