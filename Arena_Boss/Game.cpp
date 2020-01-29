e68aac79ac5589563945852e0dee2c8658cc9c94
blAs1N <qazasa123@gmail.com>
2020-01-29 21:51:03 +0900

    Build game class.

---
+++ Arena_Boss/Game.cpp
@@ -0,0 +1,34 @@
+#include "Game.h"
+#include "Accessor.h"
+#include "D3D.h"
+#include "InputManager.h"
+#include "Windows.h"
+
+Game::Game()
+	: windows(new Windows{ TEXT("Arena Boss"), 1920, 1080 }),
+	d3d(new D3D{ *windows, 1, 0 })
+{
+	Accessor<InputManager>::manager = new InputManager{ *windows };
+}
+
+Game::~Game()
+{
+	delete &Accessor<InputManager>::Get();
+}
+
+int Game::Run()
+{
+	while (true)
+	{
+		windows->ProcessEvent();
+		Accessor<InputManager>::Get().Update();
+		d3d->ClearRenderTarget();
+		d3d->ClearDepthStencil();
+
+		// Game update code
+
+		d3d->Present();
+	}
+	
+	return 0;
+}
---
+++ Arena_Boss/Game.h
@@ -0,0 +1,28 @@
+#pragma once
+
+class Game final
+{
+public:
+	Game();
+	
+	Game(const Game&) = delete;
+	Game(Game&&) noexcept = default;
+
+	Game& operator=(const Game&) = delete;
+	Game& operator=(Game&&) noexcept = default;
+	
+	~Game();
+
+	int Run();
+
+	inline class Windows& GetWindows() noexcept { return *windows; }
+	inline class D3D& GetD3D() noexcept { return *d3d; }
+
+	inline const class Windows& GetWindows() const noexcept { return *windows; }
+	inline const class D3D& GetD3D() const noexcept { return *d3d; }
+
+private:
+	Windows* windows;
+	D3D* d3d;
+};
+
