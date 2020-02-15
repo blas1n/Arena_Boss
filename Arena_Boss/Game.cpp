cc7af3645edda4d1692867601ee476e1ee144111
blAs1N <qazasa123@gmail.com>
2020-02-15 21:10:24 +0900

    Build window manager class.

--- Arena_Boss/Game.cpp
+++ Arena_Boss/Game.cpp
@@ -2,9 +2,11 @@
 #include <exception>
 #include <SDL2/SDL.h>
 #include "Accessor.h"
+#include "ConfigFile.h"
 #include "InputManager.h"
 #include "Log.h"
 #include "Util.h"
+#include "WindowManager.h"
 
 namespace ArenaBoss
 {
@@ -17,6 +19,27 @@ namespace ArenaBoss
 
 		try
 		{
+			ConfigFile config{ "Config.cfg" };
+
+			std::string name = *config("Common", "Name");
+			uint32_t width = std::stoi(*config("Common", "Width"));
+			uint32_t height = std::stoi(*config("Common", "Height"));
+			std::string screenStr = *config("Common", "ScreenMode");
+
+			ScreenMode screenMode = ScreenMode::Window;
+			if (screenStr == "FullScreen")
+				screenMode = ScreenMode::FullScreen;
+			else if (screenStr == "Borderless")
+				screenMode = ScreenMode::Borderless;
+
+		
+			Accessor<WindowManager>::manager = new WindowManager
+			{
+				name.c_str(),
+				width, height,
+				screenMode
+			};
+
 			Accessor<InputManager>::manager = new InputManager{};
 		}
 		catch (std::exception& e)
---
+++ Arena_Boss/WindowManager.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <cstdint>
+#include <SDL2/SDL.h>
+#include "Vector2.h"
+
+namespace ArenaBoss
+{
+	enum class ScreenMode
+	{
+		Window,
+		FullScreen,
+		Borderless
+	};
+
+	class WindowManager
+	{
+	public:
+		WindowManager(const char* inTitle, uint32_t inWidth,
+			uint32_t inHeight, ScreenMode inScreenMode);
+
+		~WindowManager();
+
+		inline const char* GetTitle() const noexcept
+		{
+			return title;
+		}
+
+		inline uint32_t GetWidth() const noexcept
+		{
+			return width;
+		}
+
+		inline uint32_t GetHeight() const noexcept
+		{
+			return height;
+		}
+
+		inline Math::Vector2 GetSize() const noexcept
+		{
+			return Math::Vector2
+			{
+				static_cast<float>(width),
+				static_cast<float>(height)
+			};
+		}
+
+		inline ScreenMode GetScreenMode() const noexcept
+		{
+			return screenMode;
+		}
+
+		void SetTitle(const char* inTitle) noexcept;
+		void SetSize(uint32_t inWidth, uint32_t inHeight) noexcept;
+		void SetScreenMode(ScreenMode inScreenMode) noexcept;
+
+	private:
+		SDL_Window* window;
+
+		const char* title;
+		uint32_t width;
+		uint32_t height;
+		ScreenMode screenMode;
+	};
+}
