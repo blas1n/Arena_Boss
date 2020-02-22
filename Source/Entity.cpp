7f9335463be0a2918725d34c21f96ad40e66e27d
blAs1N <qazasa123@gmail.com>
2020-02-22 22:52:41 +0900

    Add component modifier, getter.

--- Source/Entity.cpp
+++ Source/Entity.cpp
@@ -1,1 +1,16 @@
 #include "Entity.h"
+
+namespace ArenaBoss
+{
+	void Entity::Init() {}
+	void Entity::Release() {}
+
+	Component* Entity::FindComponent(const std::string& componentName)
+	{
+		for (auto& component : components)
+			if (component->Name() == componentName)
+				return component;
+
+		throw std::exception{ "No corresponding component exists." };
+	}
+}
--- Source/Entity.h
+++ Source/Entity.h
@@ -1,11 +1,71 @@
 #pragma once
 
+#include <cassert>
+#include <type_traits>
+#include <vector>
+#include "Accessor.h"
+#include "Component.h"
+#include "ComponentManager.h"
+#include "Log.h"
+
 namespace ArenaBoss
 {
-	class Entity
+	class Transform;
+
+	class Entity : private Accessor<ComponentManager>
 	{
 	public:
 		void Init();
 		void Release();
+
+		template <class ComponentType>
+		ComponentType& GetComponent() 
+		{
+			static_assert(std::is_base_of<Component, ComponentType>::value,
+				"ComponentType is not Component");
+
+			return *(static_cast<ComponentType*>(FindComponent()));
+		}
+
+		template <class ComponentType>
+		const ComponentType& GetComponent() const
+		{
+			static_assert(std::is_base_of<Component, ComponentType>::value,
+				"ComponentType is not Component");
+
+			return *(static_cast<ComponentType*>(FindComponent()));
+		}
+
+		template <>
+		inline Transform& GetComponent<Transform>() noexcept { return *transform; }
+
+		template <>
+		inline const Transform& GetComponent<Transform>() const noexcept { return *transform; }
+
+		template <class ComponentType>
+		ComponentType& AddComponent()
+		{
+			static_assert(std::is_base_of<Component, ComponentType>::value,
+				"ComponentType is not Component");
+
+			auto* component = GetManager().CreateComponent<ComponentType>();
+			components.push_back(component);
+			return component;
+		}
+
+		template <>
+		Transform& AddComponent<Transform>() noexcept
+		{
+			Log("Transform cannot be added.");
+			std::abort();
+		}
+
+	private:
+		Component* FindComponent(const std::string& componentName);
+
+	private:
+		std::string name;
+		Transform* transform;
+		std::vector<Component*> components;
 	};
 }
