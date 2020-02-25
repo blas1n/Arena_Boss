852a8839a4b296fdf102c3243eb4f2c3089627bf
blAs1N <qazasa123@gmail.com>
2020-02-25 23:26:04 +0900

    Build basic render code.

--- Source/RenderManager.cpp
+++ Source/RenderManager.cpp
@@ -39,7 +39,18 @@ namespace ArenaBoss
 	void RenderManager::Update()
 	{
 		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glDisable(GL_BLEND);
+		glEnable(GL_DEPTH_TEST);
+
+		// Render mesh
+
+		glDisable(GL_DEPTH_TEST);
+		glEnable(GL_BLEND);
+
+		glBlendEquationSeparate(GL_FUNC_ADD, GL_FUNC_ADD);
+		glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ZERO);
 
 		SDL_GL_SwapWindow(window);
 	}
