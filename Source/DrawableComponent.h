9a738d1d38587407f4c28c820483953628f38c88
blAs1N <qazasa123@gmail.com>
2020-02-26 18:48:08 +0900

    Add draw kind enum class.

--- Source/DrawableComponent.h
+++ Source/DrawableComponent.h
@@ -4,11 +4,21 @@
 
 namespace ArenaBoss
 {
+	enum class DrawKind { Sprite, Mesh };
+
 	class DrawableComponent : public Component
 	{
 		GENERATE_COMPONENT(DrawableComponent)
 
 	public:
 		virtual void Draw(class Shader* shader) = 0;
+		virtual DrawKind GetKind() const noexcept = 0;
 	};
+
+#define GENERATE_DRAWABLE_COMPONENT(name, kind) \
+GENERATE_COMPONENT(name) \
+	inline DrawKind GetKind() const noexcept override \
+	{ \
+		return kind; \
+	}
 }
