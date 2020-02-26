c93cd9ff48381bcb7de62714ca93820c53d4728b
blAs1N <qazasa123@gmail.com>
2020-02-26 23:55:09 +0900

    Add attribute of drawable component.

--- Source/DrawableComponent.h
+++ Source/DrawableComponent.h
@@ -13,6 +13,12 @@ namespace ArenaBoss
 	public:
 		virtual void Draw(class Shader* shader) = 0;
 		virtual DrawKind GetKind() const noexcept = 0;
+
+		inline bool IsVisible() const noexcept { return visible; }
+		inline void SetVisible(bool inVisible) noexcept { visible = inVisible; }
+
+	private:
+		bool visible = true;
 	};
 
 #define GENERATE_DRAWABLE_COMPONENT(name, kind) \
