c6a1bb0daac2aafe666ad7aae7e6dce05dd2532b
blAs1N <qazasa123@gmail.com>
2020-02-26 16:25:36 +0900

    Use argument instead of member variable.

--- Source/DrawableComponent.h
+++ Source/DrawableComponent.h
@@ -4,19 +4,11 @@
 
 namespace ArenaBoss
 {
-	class Shader;
-
 	class DrawableComponent : public Component
 	{
 		GENERATE_COMPONENT(DrawableComponent)
 
 	public:
-		virtual void Draw() = 0;
-
-		Shader* GetShader() noexcept { return shader; }
-		void SetShader(Shader* inShader) noexcept { shader = inShader; }
-
-	private:
-		Shader* shader;
+		virtual void Draw(class Shader* shader) = 0;
 	};
 }
