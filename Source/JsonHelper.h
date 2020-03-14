ced0395bc79f72be9a97441f24383a77ebfec709
blAs1N <qazasa123@gmail.com>
2020-03-15 00:03:59 +0900

    Use namespace in json helper.

--- Source/JsonHelper.h
+++ Source/JsonHelper.h
@@ -15,37 +15,33 @@ namespace ArenaBoss
 	{
 		struct JsonSaver final
 		{
-		public:
 			JsonSaver(Allocator& inAlloc, Object& inObject)
 				: alloc(inAlloc), object(inObject) {}
 
 			JsonSaver(JsonSaver& other, Object& inObject)
 				: alloc(other.alloc), object(inObject) {}
 
-		private:
-			friend struct JsonHelper;
-
 			Allocator& alloc;
 			Object& object;
 		};
 
-		struct JsonHelper final
+		namespace JsonHelper
 		{
-			static std::optional<int> GetInt(const Object& object, const char* name);
-			static std::optional<float> GetFloat(const Object& object, const char* name);
-			static std::optional<std::string> GetString(const Object& object, const char* name);
-			static std::optional<bool> GetBool(const Object& object, const char* name);
-			static std::optional<Math::Vector2> GetVector2(const Object& object, const char* name);
-			static std::optional<Math::Vector3> GetVector3(const Object& object, const char* name);
-			static std::optional<Math::Rotator> GetRotator(const Object& object, const char* name);
+			std::optional<int> GetInt(const Object& object, const char* name);
+			std::optional<float> GetFloat(const Object& object, const char* name);
+			std::optional<std::string> GetString(const Object& object, const char* name);
+			std::optional<bool> GetBool(const Object& object, const char* name);
+			std::optional<Math::Vector2> GetVector2(const Object& object, const char* name);
+			std::optional<Math::Vector3> GetVector3(const Object& object, const char* name);
+			std::optional<Math::Rotator> GetRotator(const Object& object, const char* name);
 
-			static void AddInt(JsonSaver& saver, const char* name, int value);
-			static void AddFloat(JsonSaver& saver, const char* name, float value);
-			static void AddString(JsonSaver& saver, const char* name, const std::string& value);
-			static void AddBool(JsonSaver& saver, const char* name, bool value);
-			static void AddVector2(JsonSaver& saver, const char* name, const Math::Vector2& value);
-			static void AddVector3(JsonSaver& saver, const char* name, const Math::Vector3& value);
-			static void AddRotator(JsonSaver& saver, const char* name, const Math::Rotator& value);
+			void AddInt(JsonSaver& saver, const char* name, int value);
+			void AddFloat(JsonSaver& saver, const char* name, float value);
+			void AddString(JsonSaver& saver, const char* name, const std::string& value);
+			void AddBool(JsonSaver& saver, const char* name, bool value);
+			void AddVector2(JsonSaver& saver, const char* name, const Math::Vector2& value);
+			void AddVector3(JsonSaver& saver, const char* name, const Math::Vector3& value);
+			void AddRotator(JsonSaver& saver, const char* name, const Math::Rotator& value);
 		};
 	}
 };
