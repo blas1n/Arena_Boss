59a8bc99acc580d95a1f954da262e30fba42077e
blAs1N <qazasa123@gmail.com>
2020-04-23 11:01:52 +0900

    Set constructor and destructor.

--- Source/RenderManager.cpp
+++ Source/RenderManager.cpp
@@ -19,51 +19,19 @@ namespace ArenaBoss
 	RenderManager::RenderManager()
 		: spriteComponents(),
 		renderTree(new RenderTree{}),
-		window(nullptr),
-		resourceManager(ResourceAccessor::GetManager()),
-		context(nullptr),
 		view(),
-		projection(),
-		width(0u),
-		height(0u)
+		projection()
 	{
-		auto& windowManager = WindowAccessor::GetManager();
-		window = windowManager.GetWindow();
-		width = windowManager.GetWidth();
-		height = windowManager.GetHeight();
-
 		view = Math::Matrix4x4::CreateLookAt(Math::Vector3::ZERO(), Math::Vector3::BACKWARD(), Math::Vector3::UP());
 
 		projection = Math::Matrix4x4::CreateOrtho(static_cast<float>(width), 
 			static_cast<float>(height), 25.0f, 10000.0f);
 
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-
-		SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-		SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-		SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-		SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
-
-		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-		SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
-
-		context = SDL_GL_CreateContext(window);
-
 		glewExperimental = GL_TRUE;
 		if (glewInit() != GLEW_OK)
 			throw std::exception{ "Failed to initialize GLEW" };
 
-		glGetError();
-
 		GenerateSpriteResource();
-	}
-
-	RenderManager::~RenderManager()
-	{
-		SDL_GL_DeleteContext(context);
 	}
 
 	void RenderManager::Draw()
